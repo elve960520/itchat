e762bf65ffc7abf98d8972e38bfb2f0016e641ef
meng <elve960520@gmail.com>
2017-07-17 08:04:16 +0800

    last push

--- chat_test.py
+++ chat_test.py
@@ -6,37 +6,53 @@ from itchat.content import *
 
 user_name_list = []
 #注册群消息（本注册函数仅支持文本消息）
-@itchat.msg_register(TEXT, isGroupChat = True)
-def text_reply(msg):
-    print(msg['isAt'])#返回是否艾特自己
-    print(msg['ActualNickName'])#返回微信昵称（群昵称）
-    print(msg['Content'])#返回文字内容（貌似支持表情）
-    if (msg['ActualNickName'] == 'yourname'):
-        temp = str(msg['Content'])
-        username = itchat.search_chatrooms(name='qunming')
-        user = username[0]['UserName']
-        itchat.send(temp,toUserName=user)
+# @itchat.msg_register(TEXT, isGroupChat = True)
+# def text_reply(msg):
+#     print(msg['isAt'])#返回是否艾特自己
+#     print(msg['ActualNickName'])#返回微信昵称（群昵称）
+#     print(msg['Content'])#返回文字内容（貌似支持表情）
+#     if (msg['ActualNickName'] == 'yourname'):
+#         temp = str(msg['Content'])
+#         username = itchat.search_chatrooms(name='qunming')
+#         user = username[0]['UserName']
+#         itchat.send(temp,toUserName=user)
 
-# @itchat.msg_register([PICTURE,RECORDING,TEXT,MAP,CARD,NOTE,SHARING,ATTACHMENT,VIDEO])
+@itchat.msg_register([PICTURE,RECORDING,TEXT,MAP,CARD,NOTE,SHARING,ATTACHMENT,VIDEO])
 #注册接受消息类型;    图片    录音      文字 位置名片 通知 分享    文件       视频
-# def simple_reply(msg):
-#     global i
-#     if msg['Type'] == TEXT:
-#         if (user_name_list.count(msg['FromUserName'])==0):
-#             user_name_list.append(msg['FromUserName'])
-#             itchat.send_msg('你好，我是孟凡宇的助理，孟凡宇正在复习，如果本条信息很紧急请致电17640226057', toUserName=msg['FromUserName'])
-#         elif (user_name_list.count(msg['FromUserName'])>0 and user_name_list.count(msg['FromUserName'])<10):
-#             user_name_list.append(msg['FromUserName'])
-#             itchat.send_msg('又见面了' , toUserName=msg['FromUserName'])
-#         elif (user_name_list.count(msg['FromUserName'])>=10):
-#             pass
+def simple_reply(msg):
+    global i
+    if msg['Type'] == TEXT:
+        if (user_name_list.count(msg['FromUserName'])==0):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('这个微信手机上没卡[捂脸]，如果着急可以添加我另一个微信:17640226057，晚上回来才能收到消息[机智]', toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>0 and user_name_list.count(msg['FromUserName'])<10):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('再发一条我也看不到，因为这是自动回复，[嘿哈]' , toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>=10):
+            pass
         # if msg['Text']  == '1':
         #     itchat.send_msg('aaaa',toUserName=msg['FromUserName'])
         # itchat.send_msg('说出来你可能不信，但这是一条自动回复',toUserName=msg['FromUserName'])
         # ReplyContent = 'i receive message:' + msg['Content']
-    # elif msg['Type'] == PICTURE:
+    elif msg['Type'] == PICTURE:
+        if (user_name_list.count(msg['FromUserName'])==0):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('这个微信手机上没卡[捂脸]，如果着急可以添加我另一个微信:17640226057，晚上回来才能收到消息[机智]', toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>0 and user_name_list.count(msg['FromUserName'])<10):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('再发一条我也看不到，因为这是自动回复，[嘿哈]' , toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>=10):
+            pass
     #     # ReplyContent = 'i receive picture:' + msg['FileName']
-    # elif msg['Type'] == RECORDING:
+    elif msg['Type'] == RECORDING:
+        if (user_name_list.count(msg['FromUserName'])==0):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('这个微信手机上没卡[捂脸]，如果着急可以添加我另一个微信:17640226057，晚上回来才能收到消息[机智]', toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>0 and user_name_list.count(msg['FromUserName'])<10):
+            user_name_list.append(msg['FromUserName'])
+            itchat.send_msg('再发一条我也看不到，因为这是自动回复，[嘿哈]' , toUserName=msg['FromUserName'])
+        elif (user_name_list.count(msg['FromUserName'])>=10):
+            pass
     #
     # elif msg['Type'] == VIDEO:
     #
@@ -62,9 +78,9 @@ def text_reply(msg):
 
 # itchat.send('Hello, filehelper', toUserName='filehelper')
 
-itchat.auto_login(hotReload=True)
+# itchat.auto_login(hotReload=True)
 
 #使用命令行二维码
-#itchat.auto_login(enableCmdQR=True)
+itchat.auto_login(enableCmdQR=True,hotReload=True)
 
 itchat.run()
